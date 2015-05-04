390a7ffae2a92b75c80b992221021e33cdb6009b
Patrick Best <patrickbest@patrickbest.com>
2015-05-04 16:25:34 -0500

    add background image & header links for news

--- partials/index-post.php
+++ partials/index-post.php
@@ -2,8 +2,20 @@
   global $post;
   $meta = get_post_meta($post->ID);
 
+  $img = false;
+  if ($thumb_id = get_post_thumbnail_id($post->ID)) {
+    $img = wp_get_attachment_image_src($thumb_id, 'size');
+  }
+
   if (!empty($meta['_igv_color'][0])) {
     $color = $meta['_igv_color'][0];
+  }
+
+  $link = false;
+  if (!empty($meta['_igv_project_link'][0])) {
+    $link = get_permalink($meta['_igv_project_link'][0]);
+  } else if (!empty($meta['_igv_external_link'][0])) {
+    $link = $meta['_igv_external_link'][0];
   }
 
   if (!empty($post->post_content)) {
@@ -17,23 +29,44 @@
     echo 'style="background-color: ' . $color .'"';
   }
 ?>>
-      <h2 class="js-post-toggle u-pointer">
-        <?php the_title(); ?>
-<?php
-  if ($has_copy) {
-?>
-        <span class="post-toggle">+</span>
-<?php
-  }
-?>
-      </h2>
 
 <?php
   if ($has_copy) {
 ?>
+      <h2 class="post-title js-post-toggle u-pointer"<?php
+        if ($img) {
+          echo 'style="background-image: url(' . $img[0] .')"';
+        }
+      ?>>
+        <?php the_title(); ?>
+        <span class="post-toggle">+</span>
+      </h2>
+
       <div class="post-copy">
         <?php the_content(); ?>
       </div>
+<?php
+  } else if ($link) {
+?>
+      <a href="<?php echo $link; ?>">
+        <h2 class="post-title"<?php
+          if ($img) {
+            echo 'style="background-image: url(' . $img[0] .')"';
+          }
+        ?>>
+          <?php the_title(); ?>
+        </h2>
+      </a>
+<?php
+  } else {
+?>
+      <h2 class="post-title"<?php
+        if ($img) {
+          echo 'style="background-image: url(' . $img[0] .')"';
+        }
+      ?>>
+        <?php the_title(); ?>
+      </h2>
 <?php
   }
 ?>
