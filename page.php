208d7ed2f271fe516a1f7529214e33a95f2c022e
Patrick Best <patrickbest@patrickbest.com>
2015-06-15 17:45:42 -0500

    add button class & add page template

---
+++ page.php
@@ -0,0 +1,42 @@
+<?php
+get_header();
+?>
+
+<!-- main content -->
+
+<main id="main-content">
+
+<?php
+if( have_posts() ) {
+  while( have_posts() ) {
+    the_post();
+?>
+
+    <article <?php post_class(); ?> id="project">
+      <header id="project-header" class="inner-container">
+        <h1 class="u-align-center"><?php the_title(); ?></h1>
+      </header>
+
+      <section id="page-copy" class="text-container">
+         <div class="project-copy-section" id="project-copy-main">
+          <?php the_content(); ?>
+        </div>
+      </section>
+
+    </article>
+
+<?php
+  }
+} else {
+?>
+    <article class="u-alert"><?php _e('Sorry, no project matched your criteria'); ?></article>
+<?php
+} ?>
+
+<!-- end main-content -->
+
+</main>
+
+<?php
+get_footer();
+?>
